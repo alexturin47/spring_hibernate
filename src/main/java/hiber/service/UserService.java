41dc95f15e8ebf06923485bd42c5fb66fdc5db2a
Alexander Tyurin <alexturin@gmail.com>
2021-11-08 23:43:39 +0700

    рабочий

--- src/main/java/hiber/service/UserService.java
+++ src/main/java/hiber/service/UserService.java
@@ -1,10 +1,21 @@
 package hiber.service;
 
+import hiber.model.Car;
 import hiber.model.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface UserService {
+    @Transactional
     void add(User user);
+
+    @Transactional
+    void add(User user, Car car);
+
     List<User> listUsers();
+
+    List<Object[]> listUsersWithCar();
+
+    User getUserByCar(Car car);
 }
